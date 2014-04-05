3c1ded8906edce7b543507365c9a227d19c38284
Mana <mana.kpp@gmail.com>
2014-04-05 16:21:22 +1300

    Trying to write user authentication code.

---
+++ RPGclassroom/Assets/Scripts/LoginScreen.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoginScreen : MonoBehaviour 
+{
+    public UserAuthentication userAuthentication;
+    public string studentScene = "studentMain";
+    public string teacherScene = "teacherMain";
+
+    public UILabel usernameField;
+    public UILabel passwordField;
+
+    public void AttemptLogin()
+    {
+        bool loggedIn = userAuthentication.AttemptLogin(usernameField.text, passwordField.text);
+
+        if (loggedIn)
+        {
+            Application.LoadLevel(studentScene);
+        }
+    }
+}
--- RPGclassroom/Assets/Scripts/UserAuthentication.cs
+++ RPGclassroom/Assets/Scripts/UserAuthentication.cs
@@ -11,6 +11,16 @@ public class UserAuthentication : MonoBehaviour
 
     private bool canLogin;
 
+    public bool AttemptLogin(string name, string password)
+    {
+        username = name;
+        this.password = password;
+
+        CheckPassword();
+
+        return canLogin;
+    }
+
     IEnumerator CheckPassword()
     {
         // Set the WWWFORM to send to the PHP Page
@@ -22,12 +32,8 @@ public class UserAuthentication : MonoBehaviour
         var getData = new WWW(loginURL, sendLoginInfo);
         yield return getData; // Wait for the data return
         bool.TryParse(getData.text, out canLogin); // Check if the data contains TRUE flag
-
-        if (canLogin)
-        {
-            Application.LoadLevel(sceneToLoad);
-        }
-        else
+        
+        if (!canLogin)
         {
             Debug.LogWarning("Invalid username or password.");
         }
