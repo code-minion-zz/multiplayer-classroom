ca51d3884bb8b9b5d1673e1d9fe336e110b94fcb
Mana <mana.kpp@gmail.com>
2014-04-05 13:27:09 +1300

    2d assets

---
+++ RPGclassroom/Assets/Scripts/UserAuthentication.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UserAuthentication : MonoBehaviour 
+{
+    // User? Student, Teacher
+    // Username
+    // Password
+
+    public string username;
+    public string password;
+
+    public void CheckPassword()
+    {
+        // Set the WWWFORM to send to the PHP Page
+        var sendLoginInfo = new WWWForm();
+        sendLoginInfo.AddField("Username", username);
+        sendLoginInfo.AddField("Password", password);
+
+        // Send the WWWForm via WWW
+    }
+}
